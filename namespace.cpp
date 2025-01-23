2b1620ccbd46fecc612b32b71ec94d6a145ff208
Mr-Taufik <taozk4417@gmail.com>
2025-01-23 22:28:38 +0800

    Update namespace.cpp

--- namespace.cpp
+++ namespace.cpp
@@ -1,3 +1,7 @@
+这段代码展示了C++中的**命名空间（namespace）**的使用。命名空间用于组织代码，避免不同部分的代码命名冲突。
+这里创建了两个命名空间：namespace1 和 namespace2，它们各自有一个 age 和 name 变量
+
+
 #include <iostream>
 using namespace std;
 
@@ -21,3 +25,6 @@ int main()
      cout << namespace2::name << endl;
      return 0;
 }
+
+在实际的项目中，可能会有多个模块或者库包含相同名称的变量、函数或类。使用命名空间能够有效避免这些命名冲突。例如，
+在这个例子中，age 和 name 在两个命名空间中定义，虽然它们的名字相同，但因为它们位于不同的命名空间中，所以它们不会冲突。
